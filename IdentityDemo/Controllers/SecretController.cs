d759491b2240bcefb6da1df22a7fa14edacca7ab
pdo9141 <pdo9141@gmail.com>
2016-09-20 21:15:10 -0700

    identity

---
+++ IdentityDemo/Controllers/SecretController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace IdentityDemo.Controllers
+{
+    [Authorize(Users = "pdo9141@gmail.com")]
+    //[Authorize(Roles = "admin, sales")]
+    public class SecretController : Controller
+    {   
+        public ContentResult Secret()
+        {
+            return Content("This is a secret...");
+        }
+
+        [AllowAnonymous]
+        public ContentResult Overt()
+        {
+            return Content("This is not a secret...");
+        }
+    }
+}
